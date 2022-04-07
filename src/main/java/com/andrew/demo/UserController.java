00f29033bd3c416be5b0ede56266a9ba749209d6
L1m1tz <andreew.mk@gmail.com>
2022-04-07 20:25:22 +0200

    created a Spring MVC REST controller that actually produces hypermedia-powered content!

--- src/main/java/com/andrew/demo/UserController.java
+++ src/main/java/com/andrew/demo/UserController.java
@@ -2,20 +2,37 @@ package com.andrew.demo;
 
 
 import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 
 @RestController
 public class UserController {
 
     private final  UserRepository repository;
+    private final UserModelAssembler assembler;
 
-    UserController(UserRepository repository){
+    UserController(UserRepository repository, UserModelAssembler assembler){
+
         this.repository = repository;
+        this.assembler =assembler;
     }
 
     @GetMapping("/users")
-    List<User> all(){
-        return repository.findAll();
+    CollectionModel<EntityModel<User>> all(){
+
+        List<EntityModel<User>> users = repository.findAll().stream()
+                .map(assembler::toModel)
+                .collect(Collectors.toList());
+        return CollectionModel.of(users,
+                        linkTo(methodOn(UserController.class).all()).withSelfRel());
+        
     }
 
     @PostMapping("/user")
@@ -23,14 +40,17 @@ public class UserController {
         return repository.save(newUser);
     }
 
-    @GetMapping("/user/{id}")
-    User one(@PathVariable Long id) {
+    //single user
+    @GetMapping("/users/{id}")
+    EntityModel<User> one(@PathVariable Long id) {
 
-        return repository.findById(id)
-                .orElseThrow(()->new UserNotFoundException(id));
+        User user = repository.findById(id) //
+                .orElseThrow(() -> new UserNotFoundException(id));
+
+        return assembler.toModel(user);
     }
 
-    @PutMapping("/user/{id}")
+    @PutMapping("/users/{id}")
     User replaceUser(@RequestBody User newUser, @PathVariable Long id){
         return repository.findById(id).map(user -> {
                     user.setFirstName(newUser.getFirstName());
